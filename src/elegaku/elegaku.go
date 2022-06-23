666c2d7115f3693155193bf80ce485ae25a60759
rikuto110511 <rikuto.511@docomo.ne.jp>
2022-06-23 19:29:01 +0900

    テーブル定義を追加

--- src/elegaku/elegaku.go
+++ src/elegaku/elegaku.go
@@ -1,13 +1,33 @@
 package elegaku
 
-import "fmt"
-
-// 仮置き
-type Rank struct {
-	Rank   int    `json:"rank"`
-	GirlId string `json:"girl_id"`
+type Girl struct {
+	GirlId         string `dynamo:"girl_id,hash"`
+	Name           string `dynamo:"name"`
+	Age            int    `dynamo:"age"`
+	ThreeSize      string `dynamo:"three_size"`
+	CatchCopy      string `dynamo:"catch_copy"`
+	Image          string `dynamo:"image"`
+	CreateDatetime string `dynamo:"create_datetime"`
+	UpdateDatetime string `dynamo:"update_datetime"`
 }
 
-func info() {
-	fmt.Println("elegaku pkg")
+type NewFace struct {
+	GirlId         string `dynamo:"girl_id,hash"`
+	CreateDatetime string `dynamo:"create_datetime"`
+	UpdateDatetime string `dynamo:"update_datetime"`
+}
+
+type Rank struct {
+	Rank           int    `dynamo:"rank,hash"`
+	GirlId         string `dynamo:"girl_id"`
+	CreateDatetime string `dynamo:"create_datetime"`
+	UpdateDatetime string `dynamo:"update_datetime"`
+}
+
+type Schedule struct {
+	GirlId         string `dynamo:"girl_id,hash"`
+	Time           string `dynamo:"time"`
+	NoticeFlg      int    `dynamo:"notice_flg"`
+	CreateDatetime string `dynamo:"create_datetime"`
+	UpdateDatetime string `dynamo:"update_datetime"`
 }
