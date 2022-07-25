2356f88afce6c575e1ebb84df75cae50a69bd752
rikuto110511 <rikuto.511@docomo.ne.jp>
2022-07-26 00:35:03 +0900

    test

--- src/elegaku/eventPostback.go
+++ src/elegaku/eventPostback.go
@@ -56,166 +56,107 @@ func Postback(bot *linebot.Client, event *linebot.Event) {
 // お気に入り登録用リストを送信
 func notificationList(bot *linebot.Client, event *linebot.Event) {
 	jsonData := []byte(`{
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "action": {
-			"type": "uri",
-			"uri": "http://linecorp.com/"
-		  }
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "contents": [
+		type: 'carousel',
+		contents: [
 			{
-			  "type": "text",
-			  "text": "Brown Cafe",
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "margin": "md",
-			  "contents": [
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-				},
-				{
-				  "type": "text",
-				  "text": "4.0",
-				  "size": "sm",
-				  "color": "#999999",
-				  "margin": "md",
-				  "flex": 0
+				type: 'bubble',
+				header: {
+					type: 'box',
+					layout: 'vertical',
+					contents: [
+					  {
+						type: 'text',
+						text: 'お気に入り登録',
+						align: 'center',
+						size: 'xl',
+						weight: 'bold',
+						color: '#F0F0F0',
+					  },
+					],
+					backgroundColor: '#84dcfd',
+				  },
+				body: {
+					type: 'box',
+					layout: 'vertical',
+					contents: [
+						{
+							type: 'separator',
+						},
+						{
+							type: 'box',
+							layout: 'horizontal',
+							contents: [
+							  {
+								type: 'image',
+								url: "https://cdn-fu-kakumei.com/image/c589c70bcc1c290e/0/0/.api",
+								align: 'start',
+								size: 'xs',
+								flex: 1,
+							  },
+							  {
+								type: 'box',
+								layout: 'vertical',
+								contents: [
+								  {
+									type: 'text',
+									text: "girlNameAndAge",
+									weight: 'bold',
+									action: {
+									  type: 'uri',
+									  label: 'action',
+									  uri:
+										'https://www.elegaku.com/profile/top/castCode/260317/',
+									},
+									color: '#3db3e9',
+								  },
+								  {
+									type: 'text',
+									text: threeSize,
+									size: 'xs',
+								  },
+								  {
+									type: 'text',
+									text: catchCopy,
+									size: 'xs',
+								  },
+								],
+								flex: 2,
+							  },
+							  {
+								type: 'button',
+								action: {
+								  type: 'postback',
+								  label: '解除',
+								  data: 'register:remove=girlId',
+								},
+								flex: 1,
+								position: 'relative',
+								gravity: 'center',
+								style: 'primary',
+								color: '#F30100',
+								adjustMode: 'shrink-to-fit',
+							  }
+							],
+							margin: 'none',
+							paddingTop: 'sm',
+						  }
+	
+					]
 				}
-			  ]
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "margin": "lg",
-			  "spacing": "sm",
-			  "contents": [
-				{
-				  "type": "box",
-				  "layout": "baseline",
-				  "spacing": "sm",
-				  "contents": [
-					{
-					  "type": "text",
-					  "text": "Place",
-					  "color": "#aaaaaa",
-					  "size": "sm",
-					  "flex": 1
-					},
-					{
-					  "type": "text",
-					  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-					  "wrap": true,
-					  "color": "#666666",
-					  "size": "sm",
-					  "flex": 5
-					}
-				  ]
-				},
-				{
-				  "type": "box",
-				  "layout": "baseline",
-				  "spacing": "sm",
-				  "contents": [
-					{
-					  "type": "text",
-					  "text": "Time",
-					  "color": "#aaaaaa",
-					  "size": "sm",
-					  "flex": 1
-					},
-					{
-					  "type": "text",
-					  "text": "10:00 - 23:00",
-					  "wrap": true,
-					  "color": "#666666",
-					  "size": "sm",
-					  "flex": 5
-					}
-				  ]
-				}
-			  ]
 			}
-		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "style": "link",
-			  "height": "sm",
-			  "action": {
-				"type": "uri",
-				"label": "CALL",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "style": "link",
-			  "height": "sm",
-			  "action": {
-				"type": "uri",
-				"label": "WEBSITE",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "contents": [],
-			  "margin": "sm"
-			}
-		  ],
-		  "flex": 0
-		}
+		]
 	  }
 	`)
 
 	container, err := linebot.UnmarshalFlexMessageJSON(jsonData)
 	if err != nil {
 		// 正しくUnmarshalできないinvalidなJSONであればerrが返る
-		panic(err)
+		message := linebot.NewTextMessage(err.Error())
+		bot.ReplyMessage(event.ReplyToken, message).Do()
+	} else {
+		message := linebot.NewFlexMessage("alt text", container)
+		bot.ReplyMessage(event.ReplyToken, message).Do()
 	}
-	message := linebot.NewFlexMessage("alt text", container)
-	bot.ReplyMessage(event.ReplyToken, message).Do()
 }
 
 // お気に入り登録し、結果を送信
